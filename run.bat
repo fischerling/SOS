4499f54c2132c50aeb6e0b10e9e6344cb46e605a
Florian Fischer <florian.fischer@muhq.space>
2024-02-18 21:23:51 +0100

    fix the path to the MentOS bootloader

--- run.bat
+++ run.bat
@@ -1,1 +1,1 @@
-qemu\qemu-system-i386 -L qemu\pc-bios -serial stdio -vga std -m 1096M -drive file=MentOS\rootfs.img,format=raw,index=0,media=disk -kernel MentOS\bootloader.bin
+qemu\qemu-system-i386 -L qemu\pc-bios -serial stdio -vga std -m 1096M -drive file=MentOS\rootfs.img,format=raw,index=0,media=disk -kernel MentOS\mentos\bootloader.bin
